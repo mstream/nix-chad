52bd84cb84a09fc85c3c967eb5aa0caa2a862324
mstream <maciej.laciak@gmail.com>
2024-05-06 16:40:15 +0100

    fix issue of stale Launchpad application shortcuts

--- packages/switch/default.nix
+++ packages/switch/default.nix
@@ -1,6 +1,18 @@
 { pkgs, system, ... }:
 let
-  script =
-    "\n    set -e\n    nix build --experimental-features 'nix-command flakes' --show-trace '.#darwinConfigurations.macbook.${system}.system'\n    ./result/sw/bin/darwin-rebuild switch --flake '.#macbook.${system}'\n  ";
+  script = ''
+    set -e
+
+    nix build \
+      --experimental-features 'nix-command flakes' \
+      --show-trace \
+      '.#darwinConfigurations.macbook.${system}.system
+
+    defaults write com.apple.dock ResetLaunchPad -bool true 
+
+    kilall Dock 
+
+    ./result/sw/bin/darwin-rebuild switch --flake '.#macbook.${system}
+  '';
   scriptBin = pkgs.writeShellScriptBin "switch" script;
 in scriptBin
