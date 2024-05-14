3ec82a0cb0bbf380a43ff784b44bd527c2e5c58a
mstream <maciej.laciak@gmail.com>
2024-05-14 08:29:58 +0100

    add a few system options

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -30,6 +30,11 @@ in {
     historySubstringSearch = { };
     initExtra = ''
       unsetopt extended_glob
+      setopt ignoreeof
+      bindkey -M vicmd "^D" down-history 
+      bindkey -M viins "^D" down-history 
+      bindkey -M vicmd "^U" up-history 
+      bindkey -M viins "^U" up-history 
       export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
       gpgconf --launch gpg-agent
       hidutil property --set '{"UserKeyMapping":${
