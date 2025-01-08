076e335acde90da80b84d1e57af33f69718a5f46
mstream <maciej.laciak@gmail.com>
2025-01-08 22:03:48 +0000

    make vim display full relative file path in lualine

--- modules/darwin/programs/nixvim/plugins/lualine.nix
+++ modules/darwin/programs/nixvim/plugins/lualine.nix
@@ -1,13 +1,25 @@
+{ config, ... }:
+let
+  cfg = config.chad;
+  inherit (cfg.editor) documentWidth;
+in
 {
   programs.nixvim.plugins.lualine = {
     enable = true;
     settings = {
-      options.globalStatus = true;
+      options = {
+        globalStatus = true;
+        icons_enabled = true;
+      };
       sections = {
         lualine_a = [ "mode" ];
         lualine_b = [ "branch" ];
         lualine_c = [
-          "filename"
+          {
+            __unkeyed-1 = "filename";
+            path = 1;
+            shorting_target = documentWidth;
+          }
           "diff"
         ];
         lualine_x = [
