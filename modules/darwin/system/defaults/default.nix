d2ab5a0f5ae2d0a2578dd5d891a32187ecdfe87a
mstream <maciej.laciak@gmail.com>
2025-12-11 15:40:43 +0000

    adjust finder settings

--- modules/darwin/system/defaults/default.nix
+++ modules/darwin/system/defaults/default.nix
@@ -31,13 +31,25 @@ in
       GloballyEnabled = false;
     };
     finder = {
+      _FXEnableColumnAutoSizing = true;
       _FXShowPosixPathInTitle = false;
+      _FXSortFoldersFirst = true;
+      _FXSortFoldersFirstOnDesktop = true;
       AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
       CreateDesktop = false;
-      QuitMenuItem = true;
-      FXEnableExtensionChangeWarning = true;
+      FXDefaultSearchScope = "SCcf";
+      FXEnableExtensionChangeWarning = false;
       FXPreferredViewStyle = "Nlsv";
+      FXRemoveOldTrashItems = false;
+      NewWindowTarget = "Computer";
+      NewWindowTargetPath = null;
+      QuitMenuItem = true;
+      ShowExternalHardDrivesOnDesktop = false;
+      ShowHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
       ShowPathbar = true;
+      ShowRemovableMediaOnDesktop = false;
       ShowStatusBar = true;
     };
     loginwindow = {
@@ -83,6 +95,7 @@ in
   };
   imports = [
     ./dock.nix
+    ./finder.nix
     ./trackpad.nix
   ];
 }
---
+++ modules/darwin/system/defaults/finder.nix
@@ -0,0 +1,24 @@
+_: {
+  config.system.defaults.finder = {
+    _FXEnableColumnAutoSizing = true;
+    _FXShowPosixPathInTitle = false;
+    _FXSortFoldersFirst = true;
+    _FXSortFoldersFirstOnDesktop = true;
+    AppleShowAllExtensions = true;
+    AppleShowAllFiles = true;
+    CreateDesktop = false;
+    FXDefaultSearchScope = "SCcf";
+    FXEnableExtensionChangeWarning = false;
+    FXPreferredViewStyle = "Nlsv";
+    FXRemoveOldTrashItems = false;
+    NewWindowTarget = "Computer";
+    NewWindowTargetPath = null;
+    QuitMenuItem = true;
+    ShowExternalHardDrivesOnDesktop = false;
+    ShowHardDrivesOnDesktop = false;
+    ShowMountedServersOnDesktop = false;
+    ShowPathbar = true;
+    ShowRemovableMediaOnDesktop = false;
+    ShowStatusBar = true;
+  };
+}
