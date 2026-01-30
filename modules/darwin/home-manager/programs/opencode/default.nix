8576e96c43eca323050c1ad51faa1c6260b6ec53
mstream <maciej.laciak@gmail.com>
2026-01-30 13:52:16 +0000

    add opencode config

---
+++ modules/darwin/home-manager/programs/opencode/default.nix
@@ -0,0 +1,52 @@
+_: {
+  programs.opencode = {
+    enable = true;
+    settings = {
+      autoshare = false;
+      autoupdate = false;
+      compaction = {
+        auto = true;
+        prune = true;
+      };
+      model = "google/gemini-3-flash-preview";
+      permission = {
+        bash = "ask";
+        edit = "ask";
+        glob = "ask";
+        grep = "ask";
+        list = "ask";
+        lsp = "ask";
+        patch = "ask";
+        skill = "ask";
+        read = "ask";
+        todoread = "ask";
+        todowrite = "ask";
+        webfetch = "ask";
+        write = "ask";
+      };
+      plugin = [ "opencode-gemini-auth@v1.3.8" ];
+      provider = {
+        google = {
+          models = {
+            "gemini-3-flash-preview" = {
+              options = {
+                thinkingConfig = {
+                  thinkingLevel = "high";
+                  includeThoughts = true;
+                };
+              };
+            };
+          };
+        };
+      };
+      theme = "gruvbox";
+      tui = {
+        diff_style = "stacked";
+        scroll_acceleration = {
+          enabled = true;
+        };
+        scroll_speed = 3;
+      };
+    };
+  };
+}
