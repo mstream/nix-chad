42edc2e03a64d1e45669d55b0d1b6e0697d26140
mstream <maciej.laciak@gmail.com>
2026-03-07 11:15:09 +0000

    wip

--- outputs/public/flake.nix
+++ outputs/public/flake.nix
@@ -28,7 +28,7 @@
     nix-rosetta-builder = {
       url = "github:cpick/nix-rosetta-builder?rev=50e6070082e0b4fbaf67dd8f346892a1a9ed685c";
       # TODO: remove when logind.settings is backported to the darwin branch of nixpkgs
-      # inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs?rev=fabb8c9deee281e50b1065002c9828f2cf7b2239";
     nixvim = {
