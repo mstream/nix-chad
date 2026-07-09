4d331598e0e9ecd6f5006416c8855a224ca29094
mstream <maciej.laciak@gmail.com>
2026-07-09 09:27:55 +0100

    enable zsh bash compatibility module

--- modules/darwin/home-manager/programs/zsh/default.nix
+++ modules/darwin/home-manager/programs/zsh/default.nix
@@ -42,7 +42,11 @@ in
       enable = true;
     };
     cdpath = [ ];
-    completionInit = "autoload -U compinit && compinit";
+    completionInit = ''
+      autoload -Uz compinit && compinit
+      autoload -Uz bashcompinit && bashcompinit
+      source <(dhall --bash-completion-script dhall)
+    '';
     dirHashes = { };
     enable = true;
     enableCompletion = true;
